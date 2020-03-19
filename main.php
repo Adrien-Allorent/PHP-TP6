7ffbe430146dc6eb9e0da752f8ccac5cf2df6486
Adrien <adrien.allorent@gmail.com>
2020-03-19 11:47:17 +0100

    Commit 2

--- main.php
+++ main.php
@@ -1,7 +1,20 @@
-<?php
-echo "Hello World!";
-?>
+<h1>TP6</h1>
+<a href="main.php">Touche F5</a>
+<h2>Exo1</h2>
 
 <?php
 
+class FootballTeam{
+    public $name;
+    public $nbTitres;
+    function display(){
+        echo "L'équipe ". $this->name ." a ". $this->nbTitres ." titres.";
+    }
+}
+
+$Canaris = new FootballTeam();
+$Canaris->name = "Les Canaris de la Beaujoire";
+$Canaris->nbTitres = 666;
+$Canaris->display();
 ?>
+
