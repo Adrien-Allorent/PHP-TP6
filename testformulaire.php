24b654eea8f06ee93e10970bb0a9ea7316bad771
Adrien <adrien.allorent@gmail.com>
2020-03-24 17:33:05 +0100

    Commit 4

--- testformulaire.php
+++ testformulaire.php
@@ -1,11 +1,14 @@
 <?php
 include 'formulaire.php';
 
-$form1 = new Formulaire("testformulaire.php", "get");
-$form1->addTextZone("Votre nom : ");
-$form1->addTextZone("Votre age : ");
+$form1 = new Formulaire("testformulaire.php", "post");
+$form1->addTextZone("name");
+$form1->addTextZone("age");
 $form1->addButton("Envoi");
 $form1->getForm();
 
-echo $_GET["test"];
+if(isset($_POST["name"]) && isset($_POST["age"])){
+    echo $_POST["name"]."<br>";
+    echo $_POST["age"];
+}
 ?>
